d247495365da85db73845db8a1b918d0831aafab
chrifer7 <chrifer7@gmail.com>
2017-11-23 14:34:50 -0500

    más experimentos

--- utils/png_to_jpg.py
+++ utils/png_to_jpg.py
@@ -8,9 +8,12 @@ def convert0():
     im = Image.open(args.dir+'/'+filename)
     rgb_im = im.convert('RGB')
     rgb_im.save(args.dir+'/'+filename.split('.')[0]+'.jpg')
+    
+    name_arr = filename.split('.')
+    name_arr = name_arr[0:len(name_arr)-1]
 
     if (args.replace):
-    	os.remove(args.dir+'/'+filename)
+    	os.remove(args.dir+'/'+'.'.join(name_arr))
 
 
 def convert1():
@@ -19,7 +22,9 @@ def convert1():
     #im.show()
     background = Image.new('RGBA', im.size, (255, 255, 255))
     alpha_composite = Image.alpha_composite(background, im)
-    alpha_composite.save(args.dir+'/'+filename.split('.')[0]+'.jpg', 'JPEG', quality=95)
+    name_arr = filename.split('.')
+    name_arr = name_arr[0:len(name_arr)-1]
+    alpha_composite.save(args.dir+'/'+'.'.join(name_arr)+'.jpg', 'JPEG', quality=95)
 
     if (args.replace):
     	os.remove(args.dir+'/'+filename)
@@ -32,7 +37,9 @@ def convert2():
     
     background = Image.new('RGB', im.size, (255, 255, 255))
     background.paste(im, mask=im.split()[3]) # 3 is the alpha channel
-    background.save(args.dir+'/'+filename.split('.')[0]+'.jpg', 'JPEG', quality=95)
+    name_arr = filename.split('.')
+    name_arr = name_arr[0:len(name_arr)-1]
+    background.save(args.dir+'/'+'.'.join(name_arr)+'.jpg', 'JPEG', quality=95)
 
     if (args.replace):
     	os.remove(args.dir+'/'+filename)
@@ -40,9 +47,13 @@ def convert2():
 
 def convert3():
   for filename in os.listdir(args.dir):
+    print(filename)
     im = Image.open(args.dir+'/'+filename)
     im2 = ImageMath.eval('im/256', {'im':im}).convert('L')
-    im2.save(args.dir+'/'+filename.split('.')[0]+'.jpg', 'JPEG', quality=95)
+    name_arr = filename.split('.')
+    name_arr = name_arr[0:len(name_arr)-1]
+    print('to: '+'.'.join(name_arr))
+    im2.save(args.dir+'/'+'.'.join(name_arr)+'.jpg', 'JPEG', quality=95)
     
     if (args.replace):
     	os.remove(args.dir+'/'+filename)
@@ -62,7 +73,10 @@ if __name__=="__main__":
     print("directories do not exist")
     sys.exit(1)
     
-  convert3()
+  #print(args)
+   
+  #convert3()
+  convert0()
 
 
 
