982a962bb28b297c6eb1dd8cd7fb2bdda688c234
chrifer7 <chrifer7@gmail.com>
2017-11-23 20:34:53 -0500

    shuffle files v2

--- utils/shuffle_dist_files.py
+++ utils/shuffle_dist_files.py
@@ -1,7 +1,25 @@
 import sys, random, os
 
-def splitdirs(files, dir1, dir2, ratio):
+def splitdirs_deprecated(files, dir1, dir2, ratio):
     shuffled = files[:]
+    random.shuffle(shuffled)
+    num = round(len(shuffled) * ratio)
+    to_dir1, to_dir2 = shuffled[:num], shuffled[num:]
+    for d in dir1, dir2:
+        if not os.path.exists(d):
+            os.mkdir(d)
+    for file in to_dir1:
+        os.symlink(file, os.path.join(dir1, os.path.basename(file)))
+    for file in to_dir2:
+        os.symlink(file, os.path.join(dir2, os.path.basename(file)))
+
+def splitdirs(data_dir, dir1, dir2, ratio):
+    files = []
+    for filename in os.listdir(data_dir):
+      files.append(os.path.join(data_dir, filename))
+      
+    shuffled = files
+    print(shuffled)
     random.shuffle(shuffled)
     num = round(len(shuffled) * ratio)
     to_dir1, to_dir2 = shuffled[:num], shuffled[num:]
@@ -15,9 +33,11 @@ def splitdirs(files, dir1, dir2, ratio):
 
 if __name__ == '__main__':
     if len(sys.argv) != 5:
-        sys.exit('Usage: {} files.txt dir1 dir2 ratio'.format(sys.argv[0]))
+        sys.exit('Usage: {} data_dir dest_dir1 dest_dir2 ratio'.format(sys.argv[0]))
     else:
-        files, dir1, dir2, ratio = sys.argv[1:]
+        #files, dir1, dir2, ratio = sys.argv[1:]
+        data_dir, dest_dir1, dest_dir2, ratio = sys.argv[1:]
         ratio = float(ratio)
-        files = open(files).read().splitlines()
-        splitdirs(files, dir1, dir2, ratio)
+        #files = open(files).read().splitlines()
+        #splitdirs_deprecated(files, dir1, dir2, ratio)
+        splitdirs(data_dir, dest_dir1, dest_dir2, ratio)
